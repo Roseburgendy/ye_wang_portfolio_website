9e69f26cc15bf6039b0e5be06bfa37f53fdcd296
Roseburgendy <rosewuxi@gmail.com>
2025-05-18 23:45:16 +0800

    Basic Set up:test pic

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import headshot from './assets/homepage/Headshot.png';
+<img src={headshot} alt="My Headshot" />
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
